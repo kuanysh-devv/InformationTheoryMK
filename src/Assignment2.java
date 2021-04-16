cc54b9f22045aec4ec28024a19aef83b1cdcfce5
kuanysh-rgb <kuanysh01012@gmail.com>
2021-04-16 11:35:13 +0600

    Fixed bugs

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -1,11 +1,21 @@
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
 
 public class Assignment2 {
+
+    static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
 
     static void getProbability(String strr)
     {
 
-        double count[] = new double[256]; //ASCII size
+        double count[] = new double[128]; //ASCII size
 
         for (int i = 0; i < strr.length(); i++) //to counting how much each letter in string by ASCII code
             count[strr.charAt(i)]++;
@@ -24,10 +34,22 @@ public class Assignment2 {
 
             if (howmuch == 1) {
                 double probability = count[strr.charAt(i)]/strr.length();
-                System.out.print(strr.charAt(i) + " - ");
-                System.out.format("%.4f", probability);
-                System.out.println();
 
+                if(strr.charAt(i)==' '){
+                    System.out.print("space" + " - ");
+                    System.out.format("%.3f", probability);
+                    System.out.println();
+                }
+                else if(strr.charAt(i)=='\n'){
+                    System.out.print("newline" + " - ");
+                    System.out.format("%.3f", probability);
+                    System.out.println();
+                }
+                else {
+                    System.out.print(strr.charAt(i) + " - ");
+                    System.out.format("%.3f", probability);
+                    System.out.println();
+                }
             }
         }
     }
@@ -38,8 +60,10 @@ public class Assignment2 {
         BufferedReader text = new BufferedReader(new FileReader(file));
 
         String strr;
-        strr = text.readLine();
-        getProbability(strr);
+        String content = Files.readString(Path.of("C:\\Users\\kuany\\Desktop\\test.txt"), StandardCharsets.US_ASCII);
+        content = content.replace("\n", "").replace("\r", "");
+        getProbability(content);
+
 
     }
 }
